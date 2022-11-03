4c15793aef299862490904bac0eb9c340d4c703d
k1b3r74rd <kibertard@gmail.com>
2022-11-04 01:36:06 +0700

    Лабораторная 9. Windows Forms

--- Lab9/Form1.Designer.cs
+++ Lab9/Form1.Designer.cs
@@ -28,13 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(282, 345);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged_1);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(309, 12);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(282, 345);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged_1);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 382);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 63);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Генерация";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(237, 382);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(137, 63);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Sort";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(456, 382);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(135, 63);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Упорядочить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(609, 460);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- Lab9/Form1.cs
+++ Lab9/Form1.cs
@@ -1,20 +1,118 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Lab9
 {
+
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void richTextBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox2_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        string[] fio = { "Истанов А.В.", "Расманов Е.И.", "Сушенко П.М.", "Ирпатова И.И", "Куренай А.П.", "Остапова Л.П.", "Лимонадов Л.П.", "Ломанова Л.Л.", "Стариков И.М.", "Вилкин И.П." };
+        string[] gr = { "4Б181", "8ОИ11", "687ЛМ", "4Б181", "СТ081", "МЛТ01", "НР099", "687ЛМ", "НР099", "8ОИ11" };
+        Random rand = new Random();
+
+        string[] str = new string[10];
+        double[] sr = new double[10];
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            for (int i = 0; i <= 9; i++)
+            {
+                str[i] = $"{fio[rand.Next(1, 10)]} {gr[rand.Next(1, 10)]}";
+            }
+            for (int i = 0; i <= 9; i++)
+            {
+                richTextBox1.AppendText($"{str[i]} \n");
+            }
+            using (var sw1 = new StreamWriter("Список.txt"))
+            {
+                for (int i = 0; i <= 9; i++)
+                {
+                    sw1.WriteLine(str[i]);
+                }
+            }
+        }
+
+        int[] c = new int[10];
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox2.Clear();
+            const int n = 10, m = 5;
+            int[,] u = new int[n, m];
+
+            for (int i = 0; i < n; i++)
+            {
+                int s = 0;
+                for (int j = 0; j < m; j++)
+                {
+                    u[i, j] = rand.Next(2, 5);
+                    sr[i] = sr[i] + u[i, j];
+                    if ((u[i, j] == 4 ^ u[i, j] == 5) && s == 0)
+                    {
+                        richTextBox2.AppendText($"{str[i]} \n");
+                        s++;
+                        c[i] = 1;
+                    }
+
+                }
+                sr[i] = sr[i] / 5;
+                if (s == 0)
+                    sr[i] = 0;
+                richTextBox1.Clear();
+                string[] arrays = File.ReadAllLines("Список.txt");
+                for (int y = 0; y <= 9; y++)
+                {
+                    richTextBox1.AppendText($"{arrays[y]}\n");
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double maxV;
+            richTextBox2.Clear();
+            for (int i = 0; i < 10; i++)
+            {
+                maxV = sr.Max();
+                for (int j = 0; j < 10; j++)
+                {
+                    if (maxV == 0)
+                    {
+                        break;
+                    }
+                    if (maxV == sr[j])
+                    {
+                        richTextBox2.AppendText($"{str[j]} {sr[j]} \n");
+                        sr[j] = 0;
+                    }
+                }
+                richTextBox1.Clear();
+                string[] arrays = File.ReadAllLines("Список.txt");
+                for (int y = 0; y <= 9; y++)
+                {
+                    richTextBox1.AppendText($"{arrays[y]}\n");
+                }
+            }
+        }
     }
 }
+
